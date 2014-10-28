ecb892bc832da6967ea064c43f7e99de84b8ffa9
jianying.wcj <jianying.wcj@taobao.com>
2014-10-28 18:45:42 +0800

    add sqrt

---
+++ Sqrt/Solution.py
@@ -0,0 +1,11 @@
+__author__ = 'jianying.wcj'
+import math
+
+class Solution:
+    def sqrt(self,x):
+        return (int)(math.sqrt(x))
+
+if __name__ == "__main__" :
+    s = Solution()
+    a = (int)(s.sqrt(4))
+    print(a)
